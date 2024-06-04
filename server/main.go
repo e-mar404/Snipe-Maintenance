538006b98a41b9243a1d39b637d53834ee6f6d3e
emar <egmarinn@gmail.com>
2024-06-03 19:20:26 -0500

    add fake data to make mock table

--- server/main.go
+++ server/main.go
@@ -23,17 +23,76 @@ func NewTemplate() *Templates {
   }
 }
 
+type Device struct {
+  AssetTag int
+  SerialNumber string 
+  Name string
+  Company string
+  Title string
+  Notes string
+  StartDate string
+  CompletionDate string
+}
+
+func NewDevice(tag int, sn, name, company, title, notes, start, completion string) Device {
+  return Device {
+    AssetTag: tag,
+    SerialNumber: sn,
+    Name: name,
+    Company: company,
+    Title: title,
+    Notes: notes,
+    StartDate: start,
+    CompletionDate: completion,
+  }
+}
+
+type Devices = []Device
+type MaintenanceTableData struct {
+  Devices Devices 
+}
+
+func NewMaintenanceTableData() MaintenanceTableData {
+  return MaintenanceTableData {
+    Devices: []Device {
+      NewDevice(
+        1234,
+        "123asd0p",
+        "Test CB",
+        "Campus 001",
+        "Broken camera",
+        "camera is blurry", 
+        "2024-06-01",
+        ""),
+
+      NewDevice(
+        5678, 
+        "567hjk0p",
+        "Test Laptop",
+        "Campus 002",
+        "Broken bottom cover",
+        "right corner broke from hinge issue",
+        "2024-06-01",
+        ""),
+    },
+  }
+}
+
 type PageData struct {
+  MaintenanceTableData MaintenanceTableData
 }
 
 func NewPageData() (PageData) {
-  return PageData{
+  return PageData {
+    MaintenanceTableData: NewMaintenanceTableData(),  
   }
 }
 
 func main() {
   e := echo.New()
   e.Use(middleware.Logger())
+
+  e.Static("/css", "css")
 
   page := NewPageData()
   e.Renderer = NewTemplate()
